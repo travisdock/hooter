b5c9f6c86e5e51611bfa96b881421452e5067ba0
Travis D <travisdock@gmail.com>
2023-01-08 12:31:26 -0700

    Add devise :lock:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'devise/registrations#edit', as: :authenticated_root
+    end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
