7390c42b4515961ec48f6ffc11c05a84e4b21c93
Travis D <travisdock@gmail.com>
2023-01-19 05:37:53 -0700

    Add user mention links to hoots

--- app/models/hoot.rb
+++ app/models/hoot.rb
@@ -1,4 +1,16 @@
 class Hoot < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+  def body_with_links
+    # find all @user mentions
+    users = body.scan(/@(\w+)/)
+    users.map!{ |username| User.find_by(username: username) }.compact!
+    # for each user, replace the @user with a link to the user's profile
+    users.each do |user|
+      body.gsub!(/@#{user.username}/, "<a href='/hooters/#{user.id}'>@#{user.username}</a>")
+    end
+    # return the hoot with the @user mentions replaced with links
+    body.html_safe
+  end
 end
