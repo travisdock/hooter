2d732369a01230bfadca6fa2f3e1f83c8ce44f72
Travis D <travisdock@gmail.com>
2023-01-21 09:50:32 -0700

    Limit likes and follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_hoots, through: :likes, source: :hoot
   has_many :followers, class_name: "Follower", foreign_key: "followed_id"
+  has_many :follower_users, through: :followers, source: :follower
   has_many :follows, class_name: "Follower", foreign_key: "follower_id"
+  has_many :following_users, through: :follows, source: :followed
 
   has_one_attached :avatar
 end
